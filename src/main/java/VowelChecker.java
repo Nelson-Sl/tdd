025c7e1b37ea46a12c9fa76d9846a4f4e0af6f56
Shen Liao <shen.liao@thoughtworks.com>
2020-08-03 10:33:10 +0800

    add no_vowels_return_origin_word

---
+++ src/main/java/VowelChecker.java
@@ -0,0 +1,5 @@
+public class VowelChecker {
+    public String check(String no_vowels_word) {
+        return no_vowels_word;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MommifierTest {
+    @Test
+    void no_vowels_return_original_word() {
+        //Given
+        VowelChecker checker = new VowelChecker();
+        //When
+        String word = "qwsdf";
+        String result = checker.check(word);
+        //Then
+        assertEquals(result,word);
+    }
 }
