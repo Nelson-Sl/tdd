a343b379187dc3f37543828c5080002b07a00629
Shen Liao <shen.liao@thoughtworks.com>
2020-08-03 11:37:56 +0800

    add empty_Words_return_words_itself

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -14,6 +14,18 @@ public class MommifierTest {
         assertEquals(result,word);
     }
 
+    @Test
+    void empty_Words_return_words_itself() {
+        //Given
+        VowelChecker checker = new VowelChecker();
+        //When
+        String word = "";
+        String result = checker.check(word);
+        //Then
+        assertEquals(result,word);
+    }
+
+    @Test
     void with_uncontinuous_vowels_return_original_word() {
         //Given
         VowelChecker checker = new VowelChecker();
@@ -23,4 +35,6 @@ public class MommifierTest {
         //Then
         assertEquals(result,word);
     }
+
+
 }
